f71b2ddd5209f51d61c68f97cf52659caad6d268
goflutterjava <goflutterjava@gmail.com>
2022-08-12 16:24:38 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ttbb/base:go AS build
+COPY . /opt/sh/compile
+WORKDIR /opt/sh/compile/pkg
+RUN go build -o filebeat_mate .
+
+
+FROM ttbb/filebeat:nake
+
+COPY docker-build /opt/sh/filebeat/mate
+
+COPY --from=build /opt/sh/compile/pkg/filebeat_mate /opt/sh/filebeat/mate/filebeat_mate
+
+WORKDIR /opt/sh/filebeat
+
+CMD ["/usr/bin/dumb-init", "bash", "-vx", "/opt/sh/filebeat/mate/scripts/start.sh"]
---
+++ pkg/fb/start.go
@@ -0,0 +1,28 @@
+package fb
+
+import (
+	"filebeat_mate/pkg/path"
+	"filebeat_mate/pkg/util"
+	"github.com/paas-mate/gutil"
+	"go.uber.org/zap"
+)
+
+func Start() {
+	startFb()
+}
+
+func startFb() {
+	err := generateFbFile()
+	if err != nil {
+		util.Logger().Error("generate filebeat config file failed ", zap.Error(err))
+		return
+	}
+	stdout, stderr, err := gutil.CallScript(path.FbStartScript)
+	util.Logger().Info("output is ", zap.String("stdout", stdout), zap.String("stderr", stderr))
+	util.Logger().Error("run start coredns scripts failed ", zap.Error(err))
+}
+
+func generateFbFile() (err error) {
+	// todo
+	return nil
+}
