78f74ea3cc5b9e2f5e5ba73954e90e07ec6cc7a9
goflutterjava <goflutterjava@gmail.com>
2023-02-14 17:23:30 +0800

    fix log location

--- docker-build/scripts/start-filebeat.sh
+++ docker-build/scripts/start-filebeat.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
+mkdir -p $FILEBEAT_HOME/logs
 bash $DIR/config-filebeat.sh
-nohup $FILEBEAT_HOME/filebeat -e -c $FILEBEAT_HOME/filebeat.yml >>$FILEBEAT_HOME/filebeat.stdout.log 2>>$FILEBEAT_HOME/filebeat.stderr.log &
+nohup $FILEBEAT_HOME/filebeat -e -c $FILEBEAT_HOME/filebeat.yml 2>>$FILEBEAT_HOME/logs/filebeat.log &
